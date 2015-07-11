38b33590ce01074bf7090e40d3a5d8d47c623dd6
Johnson Lau <gatement@gmail.com>
2015-07-11 23:20:41 +0800

    setup mongodb client

--- src/lgh/HelloWorld.java
+++ src/lgh/HelloWorld.java
@@ -1,7 +1,11 @@
 package lgh;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        MongoClient mongoClient = new MongoClient();
+        System.out.println("done2");
     }
 }
