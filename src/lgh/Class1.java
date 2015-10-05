bf4cfcd9a32236ab55ba5ca1f72b597064f1536f
Johnson Lau <gatement@gmail.com>
2015-10-05 10:30:56 +0800

    go

---
+++ src/lgh/Class1.java
@@ -0,0 +1,13 @@
+package lgh;
+
+public class Class1 {
+    /**
+     * @deprecated
+     * test deprecated method
+     */
+    @Deprecated
+    public static String test() {
+        return "this deprecated msg";
+    }
+
+}
--- src/lgh/HelloWorld.java
+++ src/lgh/HelloWorld.java
@@ -2,6 +2,8 @@ package lgh;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("==start");
+        System.out.println(Class1.test());
+        System.out.println("==end");
     }
 }
