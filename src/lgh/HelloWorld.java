749e3448ad412193e71e04382852773e02ee6887
Johnson Lau <gatement@gmail.com>
2015-10-06 21:19:25 +0800

    go

--- src/lgh/HelloWorld.java
+++ src/lgh/HelloWorld.java
@@ -1,9 +1,29 @@
 package lgh;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class HelloWorld {
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
         System.out.println("==start");
-        System.out.println(Class1.test());
+
+        List<Integer> li = Arrays.asList(1, 2, 3);
+        List<String>  ls = Arrays.asList("one", "two", "three");
+        printList(li);
+        printList(ls);
+
         System.out.println("==end");
     }
+
+    public static void printList(List<?> list) {
+        for (Object elem: list)
+            System.out.print(elem + " ");
+        System.out.println();
+    }
+
+    void foo(List<?> i) {
+        i.set(0, i.get(0));
+    }
+    
 }
