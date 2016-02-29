0539010888c003b69e1992eb82b8d1ba249cb308
Johnson Lau <gatement@gmail.com>
2016-02-29 10:18:02 +0800

    jetty

--- src/lgh/HelloWorld.java
+++ src/lgh/HelloWorld.java
@@ -1,57 +1,31 @@
 package lgh;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.Statement;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorld {
-    public static void main(String[] args) throws Exception {
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HelloWorld extends AbstractHandler 
+{
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello Johnson</h1>");
+    }
+    public static void main(String[] args) throws Exception 
+    {
         System.out.println("==start");
-
-        int result;
-        Connection conn = null;
-        String sql;
-        String url = "jdbc:mysql://localhost:3306/test?user=root&password=&useUnicode=true&characterEncoding=UTF-8";
-
-        try{
-            // init MySQL Driver
-            // com.mysql.jdbc.Driver driver = new com.mysql.jdbc.Driver();
-            // OR 
-            // new com.mysql.jdbc.Driver();
-            // OR
-            Class.forName("com.mysql.jdbc.Driver");
-
-            conn = DriverManager.getConnection(url);
-            Statement statement = conn.createStatement();
-
-            // create table
-            sql = "create table student(id int, name varchar(20), primary key(id))";
-            statement.executeUpdate(sql);
-
-            // insert
-            sql = "insert into student(id, name) values(1, 'Johnson');";
-            result = statement.executeUpdate(sql);
-            if (result != 1) System.out.println("insert failed.");
-
-            sql = "insert into student(id, name) values(2, 'Denny');";
-            result = statement.executeUpdate(sql);
-            if (result != 1) System.out.println("insert failed.");
-            
-            // select
-            sql = "select * from student";
-            ResultSet rs = statement.executeQuery(sql);
-            while(rs.next())
-            {
-                System.out.println(rs.getString(1) + " " + rs.getString(2));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null) conn.close();
-        }
-
+        Server server = new Server(8080);
+        server.setHandler(new HelloWorld());
+        server.start();
+        server.join();
         System.out.println("==end");
     }
 }
