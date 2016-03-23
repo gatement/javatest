7f897d4ea7bb5ac7ff1dd5d21beab01f6118f9b8
Johnson Lau <gatement@gmail.com>
2016-03-23 08:53:45 +0800

    go

--- src/lgh/HelloWorld.java
+++ src/lgh/HelloWorld.java
@@ -1,25 +1,46 @@
 package lgh;
 
-import java.util.List;
-import java.util.Arrays;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
-public class HelloWorld {
-    @SuppressWarnings("deprecation")
-    public static void main(String[] args) {
+public class HelloWorld 
+{
+    public static void main(String[] args) throws Exception
+    {
         System.out.println("==start");
-        System.out.println(Class1.test());
+        
 
-        List<Integer> li = Arrays.asList(1, 2, 3);
-        List<String>  ls = Arrays.asList("one", "two", "three");
-        printList(li);
-        printList(ls);
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone("GMT"));
+        cal.setMinimalDaysInFirstWeek(4);
+        Date date = cal.getTime();
 
+        System.out.println(cal.toString());
+
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd'H'HH"); 
+        System.out.println(fmt.format(date));
+
+        fmt = new SimpleDateFormat("yyyyMMdd"); 
+        System.out.println(fmt.format(date));
+
+        fmt = new SimpleDateFormat("yyyy'W'ww"); 
+        System.out.println(fmt.format(date));
+
+        fmt = new SimpleDateFormat("yyyyMM"); 
+        System.out.println(fmt.format(date));
+
+        fmt = new SimpleDateFormat("yyyy"); 
+        System.out.println(fmt.format(date));
+
+        System.out.println("minute: " + String.valueOf(cal.get(Calendar.MINUTE)));
+        System.out.println("hour: " + String.valueOf(cal.get(Calendar.HOUR_OF_DAY)));
+        System.out.println("day: " + String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
+        System.out.println("week: " + String.valueOf(cal.get(Calendar.WEEK_OF_YEAR)));
+        System.out.println("month: " + String.valueOf(cal.get(Calendar.MONTH) + 1));
+        System.out.println("year: " + String.valueOf(cal.get(Calendar.YEAR)));
+        
         System.out.println("==end");
-    }
-
-    public static void printList(List<?> list) {
-        for (Object elem: list)
-            System.out.print(elem + " ");
-        System.out.println();
     }
 }
