29398dbb92bc11b9b587e82703f52add728c07b3
Johnson Lau <gatement@gmail.com>
2016-03-23 17:30:36 +0800

    go

--- src/lgh/HelloWorld.java
+++ src/lgh/HelloWorld.java
@@ -4,22 +4,10 @@ import java.util.List;
 import java.util.Arrays;
 
 public class HelloWorld {
-    @SuppressWarnings("deprecation")
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("==start");
-        System.out.println(Class1.test());
-
-        List<Integer> li = Arrays.asList(1, 2, 3);
-        List<String>  ls = Arrays.asList("one", "two", "three");
-        printList(li);
-        printList(ls);
 
         System.out.println("==end");
-    }
-
-    public static void printList(List<?> list) {
-        for (Object elem: list)
-            System.out.print(elem + " ");
-        System.out.println();
     }
 }
